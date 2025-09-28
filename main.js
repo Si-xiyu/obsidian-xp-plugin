258a5485d6f79f8056a4b3a02a1ee5006c981323
Si-xiyu <junxiangluo0@gmail.com>
2025-09-28 16:23:34 +0800

    可以在Obsidian中运行的MVP

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-import { Plugin, Notice } from "obsidian";
-export default class XpPlugin extends Plugin {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const obsidian_1 = require("obsidian");
+class XpPlugin extends obsidian_1.Plugin {
     async onload() {
         console.log("✅ XP Progress Bar 插件已加载");
         const statusBarItemEl = this.addStatusBarItem();
         statusBarItemEl.setText("Lv.1 | 0/100 XP");
         this.addRibbonIcon("dice", "显示 XP 提示", () => {
-            new Notice("你获得了 5 点经验！");
+            new obsidian_1.Notice("你获得了 5 点经验！");
         });
     }
     onunload() {
         console.log("❌ XP Progress Bar 插件已卸载");
     }
 }
+exports.default = XpPlugin;
 //# sourceMappingURL=main.js.map
