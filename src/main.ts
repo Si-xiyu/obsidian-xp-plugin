0844bbe7d891fc12e0a2ab8f205c545b0046ab96
Si-xiyu <junxiangluo0@gmail.com>
2025-09-28 16:14:04 +0800

    将输出的main.js移动到根目录下

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ import { Plugin, Notice } from "obsidian";
 
 export default class XpPlugin extends Plugin {
   async onload() {
-    console.log("XP Progress Bar 插件已加载 ✅");
+    console.log("✅ XP Progress Bar 插件已加载");
 
-    this.addRibbonIcon("dice", "XP 插件", () => {
-      new Notice("Hello from XP Progress Bar!");
+    const statusBarItemEl = this.addStatusBarItem();
+    statusBarItemEl.setText("Lv.1 | 0/100 XP");
+
+    this.addRibbonIcon("dice", "显示 XP 提示", () => {
+      new Notice("你获得了 5 点经验！");
     });
   }
 
   onunload() {
-    console.log("XP Progress Bar 插件已卸载 ❌");
+    console.log("❌ XP Progress Bar 插件已卸载");
   }
 }
